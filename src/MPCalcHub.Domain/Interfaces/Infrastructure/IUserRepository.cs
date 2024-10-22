8c1a31a4454781347577882f4800ef55da3d5c48
gabrieldomingos96 <gabriel.domingos@atlasinovacoes.com.br>
2024-10-22 18:49:07 -0300

    Ajuste no gitignore

--- src/MPCalcHub.Domain/Interfaces/Infrastructure/IUserRepository.cs
+++
@@ -1,8 +0,0 @@
-using MPCalcHub.Domain.Entities;
-
-namespace MPCalcHub.Domain.Interfaces.Infrastructure;
-
-public interface IUserRepository : IRepository<User>
-{
-    Task<User> GetById(Guid id, bool include, bool tracking);
-}
