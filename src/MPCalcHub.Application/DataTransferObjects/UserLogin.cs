97ffebe1aae0730f293a5e1dc0e94997aac482ea
Anderson Mello <andersonsun2@gmail.com>
2024-10-27 22:33:44 -0300

    Inclusão de DataAnnotations

--- src/MPCalcHub.Application/DataTransferObjects/UserLogin.cs
+++ src/MPCalcHub.Application/DataTransferObjects/UserLogin.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
@@ -9,9 +10,13 @@ namespace MPCalcHub.Application.DataTransferObjects
     public class UserLogin
     {
         [JsonPropertyName("email")]
+        [EmailAddress(ErrorMessage = "Email inválido.")]
         public string Email { get; set; }
 
         [JsonPropertyName("password")]
+        [Required(ErrorMessage = "A senha é obrigatória.")]
+        [RegularExpression(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\W_]).{8,}$",
+        ErrorMessage = "A senha deve ter pelo menos 8 caracteres, incluindo uma letra maiúscula, um número e um caractere especial.")]
         public string Password { get; set; }
     }
 }
