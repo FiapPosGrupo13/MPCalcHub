4b38b92ebea9fdd8ece4d552a796811c98367654
Gabriel Loureiro <gabrielloureirog@gmail.com>
2024-11-03 11:25:03 -0300

    :hammer: Ajuste no ClaimsIdentity

--- src/MPCalcHub.Domain/Services/Security/TokenService.cs
+++ src/MPCalcHub.Domain/Services/Security/TokenService.cs
@@ -44,13 +44,13 @@ public class TokenService(IOptions<TokenSettings> options, IMemoryCache cache) :
         var tokenHandler = new JwtSecurityTokenHandler();
         var tokenDescriptor = new SecurityTokenDescriptor
         {
-            Subject = new ClaimsIdentity(
-            [
-                new(ClaimTypes.NameIdentifier, user.Id.ToString()),
-                new(ClaimTypes.Name, user.Name),
-                new(ClaimTypes.Email, user.Email),
-                new(ClaimTypes.Role, ((int)user.PermissionLevel).ToString())
-            ]),
+            Subject = new ClaimsIdentity(new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Name),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.Role, ((int)user.PermissionLevel).ToString())
+            }),
             Expires = DateTime.UtcNow.AddHours(_settings.ExpirationTimeHour),
             SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
         };
